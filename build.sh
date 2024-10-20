52540eeec81e76cee4175ebe85b02aa3f55c1501
CeciliaZ030 <zhangyixin319@gmail.com>
2024-10-20 16:07:12 -0300

    fix docker file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Builde Reth and Rbuilder
+echo "Building Reth and Rbuilder..."
+if ! docker build ./ -t gwyneth-reth -f ./reth/Dockerfile
+then
+    echo "Failed to build Reth."
+    exit 1
+fi
+if ! docker build ./ -t rbuilder -f ./rbuilder/Dockerfile
+then
+    echo "Failed to build Rbuilder."
+    exit 1
+fi
+
