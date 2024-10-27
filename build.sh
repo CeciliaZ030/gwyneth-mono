198fcee9bd5312645e50e627d155eb02daf4f72f
CeciliaZ030 <zhangyixin319@gmail.com>
2024-10-27 15:14:40 +0800

    rbuilder runs

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
     echo "Failed to build Reth."
     exit 1
 fi
-if ! docker build ./ -t rbuilder -f ./rbuilder/Dockerfile
+if ! docker build ./ -t gwyneth-rbuilder -f ./rbuilder/Dockerfile
 then
     echo "Failed to build Rbuilder."
     exit 1
